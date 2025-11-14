29e45ee6ca1287ef6fc80d8ba86aaa0abaadaf6e
yoshi <chris001.educat@gmail.com>
2025-11-14 22:38:04 +0800

    swap using bitwise

--- C01/ex02/ft_swap.c
+++ C01/ex02/ft_swap.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
+// void    ft_swap(int *a, int *b)
+// {
+//     int temp;
+
+//     temp = *a;
+//     *a = *b;
+//     *b = temp; 
+// }
 
 void    ft_swap(int *a, int *b)
 {
-    int temp;
-
-    temp = *a;
-    *a = *b;
-    *b = temp; 
+    if (*a != *b)
+    {
+        *a = *a ^ *b;
+        *b = *a ^ *b;
+        *a = *a ^ *b;
+    }
 }
 
 int main (void)
@@ -15,10 +24,11 @@ int main (void)
     int a = 9;
     int b = 6;
 
-    int *ptr1 = &a;
-    int *ptr2 = &b;
-
-    ft_swap(ptr1, ptr2);
+    // int *ptr1 = &a;
+    // int *ptr2 = &b;
+    //ft_swap(ptr1, ptr2);
+    
+    ft_swap(&a, &b);
     printf("%d", a);
     printf("%d", b);
     return 0;
